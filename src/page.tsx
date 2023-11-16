d0ddb9fa40c292eaa0907b71a178628d9336a82f
Temi-pinheiro <temitopepinheiro@gmail.com>
2023-11-17 00:00:36 +0100

    added maxwidth to paragraph

--- src/page.tsx
+++ src/page.tsx
@@ -80,7 +80,7 @@ export const Page = () => {
                       The Bright Future of Web 3.0?
                     </h1>
                     <aside className='flex flex-col'>
-                      <p className='text-gray-500 text-[15px]'>
+                      <p className='text-[#5E607A] text-[15px] max-w-[350px]'>
                         We dive into the next evolution of the web that claims
                         to put the power of the platforms back into the hands of
                         the people. But is it really fulfilling its promise?
